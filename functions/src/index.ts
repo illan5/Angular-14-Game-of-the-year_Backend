77222b7bb24aac966711f2e90f9181753fbccec7
Jose Illan <jose_illan@hotmail.com>
2020-05-11 22:26:20 +0200

    Configure credentials to work localy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,12 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://firestore-grafica-59a2d.firebaseio.com"
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
